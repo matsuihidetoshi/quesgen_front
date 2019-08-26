2c5a8430dcf9785f36e893c7d54dcf904c87616d
Matsui Hidetoshi <matsui@pharcyde.jp>
2019-08-27 00:46:12 +0900

    add polling

--- src/components/questions/show.vue
+++ src/components/questions/show.vue
@@ -1,13 +1,14 @@
 <template>
     <div class="show">
+        <p>{{ time }}</p>
         <p>{{ results.id }}</p>
         <p>{{ results.title }}</p>
         <p>{{ results.content }}</p>
-        <p>{{ results.option1 }}</p>
-        <p>{{ results.option2 }}</p>
-        <p>{{ results.option3 }}</p>
-        <p>{{ results.option4 }}</p>
-        <p>{{ results.option5 }}</p>
+        <div>{{ results.option1 }}:{{ results.ratio1 }}%<button v-on:click="vote(1)">vote</button></div>
+        <div>{{ results.option2 }}:{{ results.ratio2 }}%<button v-on:click="vote(2)">vote</button></div>
+        <div>{{ results.option3 }}:{{ results.ratio3 }}%<button v-on:click="vote(3)">vote</button></div>
+        <div>{{ results.option4 }}:{{ results.ratio4 }}%<button v-on:click="vote(4)">vote</button></div>
+        <div>{{ results.option5 }}:{{ results.ratio5 }}%<button v-on:click="vote(5)">vote</button></div>
     </div>
 </template>
 <script>
@@ -18,11 +19,50 @@ export default {
   },
   data(){
     return{
-      results: null
+      results: null,
+      updateTimer: null,
     }
   },
   mounted(){
-    axios({
+    this.getQuestion()
+    this.updateTimer = setInterval(function () {
+      this.getQuestion()
+    }.bind(this), 100)
+  },
+  beforeDestroy(){
+    clearInterval(this.updateTimer)
+  },
+  methods: {
+    vote: function(option){
+      axios({
+          url: 'https://quesgen.work/graphql/',
+          method: 'POST',
+          data: {
+              query:
+              `mutation {
+                  postQuestion(id:${this.results.id} option:${option}) {
+                      question {
+                          id
+                          title
+                          content
+                          option1
+                          option2
+                          option3
+                          option4
+                          option5
+                          ratio1
+                          ratio2
+                          ratio3
+                          ratio4
+                          ratio5
+                      }
+                  }
+              }`
+          },
+      }).then(response => (this.results = response.data.data.question))
+    },
+    getQuestion: function(){
+      axios({
         url: 'https://quesgen.work/graphql/',
         method: 'POST',
         data: {
@@ -37,10 +77,16 @@ export default {
                         option3
                         option4
                         option5
+                        ratio1
+                        ratio2
+                        ratio3
+                        ratio4
+                        ratio5
                     }
                 }`
-        },
-    }).then(response => (this.results = response.data.data.question))
+          },
+      }).then(response => (this.results = response.data.data.question))
+    }
   }
 }
 </script>
